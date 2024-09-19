0fd975c515780a7c61ae12618d94b09b7be0b916
Soburr <adebesinbenzenejr99@gmail.com>
2024-09-19 07:27:39 +0100

    notes

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -11,9 +11,18 @@ new class extends Component {
 }; ?>
 
 <div>
-    <ul>
+    <div class="space-y-2">
         @foreach ($notes as $note)
-           <li>{{ $note->title }}</li>
+           <x-card wire:key='{{ $note->id }}'>
+               <div class="flex justify-between">
+                  <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
+                    {{ $note->body }}
+                  </a>
+                  <div class="text-xs-gray-500">
+                    {{ \Carbon\Carbon::parse($note->send_date)->format('M-d-y') }}
+                  </div>
+               </div>
+           </x-card>
         @endforeach
-    </ul>
+    </div>
 </div>
