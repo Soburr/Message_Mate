7a492e26adea3caf26289775a05d697d6ec8181f
Soburr <adebesinbenzenejr99@gmail.com>
2024-09-21 14:49:25 +0100

    ui

---
+++ resources/views/components/ibutton.blade.php
@@ -0,0 +1,9 @@
+<div>
+   <button class="bg-gray-200 rounded-full p-2">
+      @if ($icon == 'eye')
+         <i class="fas fa-eye"></i>
+      @elseif ($icon == 'trash')
+         <i class="fas fa-trash"></i>
+      @endif
+   </button>
+</div>
--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -12,17 +12,27 @@ new class extends Component {
 
 <div>
     <div class="space-y-2">
+      <div class="grid grid-cols-2 gap-4 mt-12">
         @foreach ($notes as $note)
            <x-card wire:key='{{ $note->id }}'>
                <div class="flex justify-between">
                   <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
-                    {{ $note->body }}
+                    {{ $note->title }}
                   </a>
                   <div class="text-xs-gray-500">
                     {{ \Carbon\Carbon::parse($note->send_date)->format('M-d-y') }}
                   </div>
                </div>
+
+               <div class="flex items-end justify-between mt-4 space-x-1">
+                  <p class="text-xs">Recipient: <span class="font-semibold"> {{ $note->recipient }} </span> </p>
+                  <div>
+                    <x-ibutton icon="eye"></x-ibutton>
+                    <x-ibutton icon="trash"></x-ibutton>
+                  </div>
+               </div>
            </x-card>
-        @endforeach
+         @endforeach
+        </div>
     </div>
 </div>
