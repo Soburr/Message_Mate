9734dd48ecb593dfd6f2ed51824653c7df91217f
Soburr <adebesinbenzenejr99@gmail.com>
2024-09-29 12:38:54 +0100

    queue jobs

---
+++ app/Jobs/SendEmail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Note;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmail implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Note $note)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
